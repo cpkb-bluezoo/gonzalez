0a49a22b292eabbe6a732593541c6b33670429be
Chris Burdess <dog@gnu.org>
2025-12-25 09:21:53 +0100

    Updated documentation

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.bluezoo.util","l":"CompositeByteBuffer"},{"p":"org.bluezoo.gonzalez","l":"NamespaceScopeTracker"},{"p":"org.bluezoo.gonzalez","l":"Parser"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.bluezoo.gonzalez","l":"NamespaceScopeTracker"},{"p":"org.bluezoo.gonzalez","l":"Parser"}];updateSearchResults();
